b4305950c3bb5ba41d1b32aa13b6f09bc4865552
Rahul9766 <rbhujbal00@gmail.com>
2024-01-01 14:00:00 +0530

    updates for 2024-01-01

--- github_commit_automation.ps1
+++ github_commit_automation.ps1
@@ -32,38 +32,35 @@ for ($i=1; $i -le $num_days; $i++) {
     # Format current date as string
     $current_date_str = $current_date.ToString("yyyy-MM-dd")
     
-    # Determine if it's a commit day or not (60% chance of a commit)
-    $commit_day = Get-Random -Minimum 0 -Maximum 10
-    if ($commit_day -lt 3) {
-        # Determine the number of commits for this day
-        $num_commits = 1
-        # On random days, make more than one commit
-        if ((Get-Random -Minimum 0 -Maximum 5) -eq 0) {
-            $num_commits = Get-Random -Minimum 2 -Maximum 5  # Make 2 to 4 commits
+    # Determine the number of commits for this day (ALWAYS at least 1)
+    $num_commits = 1
+    
+    # On some days, make more than one commit (20% chance)
+    if ((Get-Random -Minimum 0 -Maximum 5) -eq 0) {
+        $num_commits = Get-Random -Minimum 2 -Maximum 4  # Make 2 to 3 additional commits
+    }
+
+    # Loop to make multiple commits
+    for ($c=1; $c -le $num_commits; $c++) {
+        # Determine the number of tasks to run (between 1 and 5)
+        $num_tasks = Get-Random -Minimum 1 -Maximum 6
+
+        # Create shuffled array of task indices
+        $indices = 0..4 | Get-Random -Count 5
+
+        # Run the selected number of tasks
+        for ($j=0; $j -lt $num_tasks; $j++) {
+            RunTask -taskNum $indices[$j] -date $current_date_str
         }
 
-        # Loop to make multiple commits
-        for ($c=1; $c -le $num_commits; $c++) {
-            # Determine the number of tasks to run (between 1 and 5)
-            $num_tasks = Get-Random -Minimum 1 -Maximum 6
+        # Git operations
+        git add .
+        git commit -m "updates for $current_date_str"
 
-            # Create shuffled array of task indices
-            $indices = 0..4 | Get-Random -Count 5
-
-            # Run the selected number of tasks
-            for ($j=0; $j -lt $num_tasks; $j++) {
-                RunTask -taskNum $indices[$j] -date $current_date_str
-            }
-
-            # Git operations
-            git add .
-            git commit -m "updates for $current_date_str"
-
-            # Set the Git committer date and amend the commit
-            $commit_date = "$current_date_str 14:00:00"
-            $env:GIT_COMMITTER_DATE = $commit_date
-            git commit --amend --no-edit --date="$commit_date"
-        }
+        # Set the Git committer date and amend the commit
+        $commit_date = "$current_date_str 14:00:00"
+        $env:GIT_COMMITTER_DATE = $commit_date
+        git commit --amend --no-edit --date="$commit_date"
     }
 
     # Move to the next day
